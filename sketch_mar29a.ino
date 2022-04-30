851214ba2c6ac62c158bfde78d6734efe8c54842
DiegoCS-97 <60472889+DiegoCS-97@users.noreply.github.com>
2022-04-29 22:23:04 -0500

    Tercer prototipo

--- sketch_mar29a.ino
+++ sketch_mar29a.ino
@@ -1,17 +1,15 @@
-#include "HX711.h"
 #include "ThingSpeak.h"
 #include "NewPing.h"
 #include "WiFi.h"
 
 //Pines para sensores
-const int LOADCELL_DOUT_PIN = 26;
-const int LOADCELL_SCK_PIN = 25;
+const int pinAsiento = 35;
 
 const int trigE = 13; // trigger pin
 const int echoE = 12; // echo pin
 
-const int trigS = 13; // trigger pin
-const int echoS = 12; // echo pin
+const int trigS = 27; // trigger pin
+const int echoS = 26; // echo pin
 
 // Conexion a internet
 const char *ssid = "GalaxyNote10"; // Nombre de la red
@@ -23,13 +21,16 @@ const char *WriteAPIKey = "PZIFTNYNUAH3G83E"; // Write API Key del canal
 WiFiClient cliente;                           // Objeto Cliente del WiFi
 
 //Instancias de los sensores
-HX711 balanza;
 NewPing sonarEntrada(trigE, echoE, 2000);
-NewPing sonarSalida(trigS, trigS, 2000);
+NewPing sonarSalida(trigS, echoS, 2000);
 
 //Cantidades a monitorear y reportar
-int personas;
-int asientos;
+int personas = 0;
+int asientos = 0;
+
+int state = 0;
+int lastState = 0;
+
 
 void setup()
 {
@@ -44,12 +45,6 @@ void setup()
   }
   Serial.println("Wifi conectado!");
   ThingSpeak.begin(cliente); // Inicializar el servidor de ThingSpeak
-
-  balanza.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  delay(250);
-
-  balanza.set_scale(397);
-  balanza.tare(10); // Hacer 10 lecturas, el promedio es la tara
 }
 
 void loop()
@@ -57,20 +52,19 @@ void loop()
 
   leerAsiento();
   leerEntrada();
+  delay(200);
   leerSalida();
 
   ThingSpeak.writeFields(channelID, WriteAPIKey);
   Serial.println("Datos enviados a ThingSpeak!");
 
-  delay(1000);
+  delay(100);
 } 
 
 void leerEntrada() {
   int distance;
   long duration;
 
-  // Esperar 1 segundo entre mediciones
-  delay(1000);
   // Obtener medicion de tiempo de viaje del sonido y guardar en variable uS
   int uS = sonarEntrada.ping_median();
   // Imprimir la distancia medida a la consola serial
@@ -80,7 +74,7 @@ void leerEntrada() {
   Serial.println("cm");
 
   //Si la distancia es menor que el ancho de la entrada, significa que una persona entró
-  if(uS / US_ROUNDTRIP_CM < 90) {
+  if(uS / US_ROUNDTRIP_CM < 20) {
     personas++;
   } 
   
@@ -91,10 +85,8 @@ void leerSalida() {
   int distance;
   long duration;
 
-  // Esperar 1 segundo entre mediciones
-  delay(1000);
   // Obtener medicion de tiempo de viaje del sonido y guardar en variable uS
-  int uS = sonarEntrada.ping_median();
+  int uS = sonarSalida.ping_median();
   // Imprimir la distancia medida a la consola serial
   Serial.print("Distancia: ");
   // Calcular la distancia con base en una constante
@@ -102,7 +94,7 @@ void leerSalida() {
   Serial.println("cm");
 
   //Si la distancia es menor que el ancho de la salida, significa que una persona salió
-  if(uS / US_ROUNDTRIP_CM < 90) {
+  if(uS / US_ROUNDTRIP_CM < 20) {
     personas--;
   } 
   
@@ -110,18 +102,21 @@ void leerSalida() {
 }
 
 void leerAsiento() {
-  float reading = 0;
-  if (balanza.is_ready())
-  {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-    float reading = balanza.get_units(10);
-    Serial.print("Peso: ");
-    Serial.print(reading);
-    Serial.println();
-    ThingSpeak.setField(2, reading); // En el campo 2 se le asigna la cantidad de personas en asientos preferenciales
-  }
-  else
-    Serial.println("HX711 not found.");
+  int ResRead = analogRead(pinAsiento);
+  Serial.print("Lectura analogica = ");
+  Serial.println(ResRead);
 
-  //TODO detectar cuándo se sienta y se para alguien en funcion del peso captado
-  //TODO sumar y restar numero de asientos preferenciales ocupados
+  state = ResRead;
+
+  if(state != lastState) {
+    if(state >= 3000) {
+      asientos++;
+    } else {
+      asientos--;  
+    }
+  }
+
+  lastState = state;
+
+  ThingSpeak.setField(2, asientos); // En el campo 1 se le asigna la cantidad de personas en el autobus
 }
